9e15d60b76677bbaf6c16d8da961459e53b5d344
DrSamNemo <dr.samuel.nemo@gmail.com>
2012-09-04 10:11:37 -0400

    More stupid changes.

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -2,10 +2,8 @@ package com.example;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.AndroidCharacter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,9 +14,6 @@ public class MyActivity extends Activity {
     private Button _clockButton = null;
     private Button _textFieldButton = null;
 
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
